2012164a46b1d723b5658d583727c0be28edfba4
Yeunsuk <71748139+Yeunsuk@users.noreply.github.com>
2025-02-21 18:55:02 +0900

    [Bronze IV] Title: R2, Time: 64 ms, Memory: 11600 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,15 @@
+import java.util.*; //3046(수학) R2
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int a = Integer.parseInt(st.nextToken());
+    int b = Integer.parseInt(st.nextToken());
+    
+    bw.write(String.valueOf(b * 2 - a));
+    bw.close();
+  }
+}
