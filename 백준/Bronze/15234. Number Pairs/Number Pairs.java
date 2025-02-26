51b3a1884b7399444e20d3f8bbd1ddff6446dab5
Yeunsuk <71748139+Yeunsuk@users.noreply.github.com>
2025-02-26 19:17:33 +0900

    [Bronze II] Title: Number Pairs, Time: 72 ms, Memory: 12020 KB -BaekjoonHub

--- 백준/Bronze/15234. Number Pairs/Number Pairs.java
+++ 백준/Bronze/15234. Number Pairs/Number Pairs.java
@@ -2,31 +2,24 @@ import java.util.*; //15234(수학) Number Pairs
 import java.io.*;
 
 public class Main {
-  static int Macro(String str) {
-    return Integer.parseInt(str);
-  }
-
   public static void main(String[] args) throws IOException {
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
-    HashMap<Integer, Integer> map = new HashMap<>();
-    int n = Macro(st.nextToken()), cnt = 0;
-    int k = Macro(st.nextToken());
+    int n = Integer.parseInt(st.nextToken()), cnt = 0;
+    int k = Integer.parseInt(st.nextToken());
+    HashSet<Integer> set = new HashSet<>();
     int[] arr = new int[n];
 
     st = new StringTokenizer(br.readLine());
     for(int i = 0; i < n; i++) {
-      arr[i] = Macro(st.nextToken());
+      arr[i] = Integer.parseInt(st.nextToken());
     }
     
-    for (int i = 0; i < n; i++) {
-      int tmp = k - arr[i]; 
-      if (map.containsKey(tmp)) {
-        cnt += map.get(tmp);
-      }
-      
-      map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+    for(int i = 0; i < n; i++) {
+      int tmp = k - arr[i];
+      if(set.contains(tmp)) cnt++;
+      set.add(arr[i]);
     }
 
     bw.write(String.valueOf(cnt));
