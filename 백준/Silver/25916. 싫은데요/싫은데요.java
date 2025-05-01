a5fdd82f74021df25d365c7cc4058ebb6af4f5a9
eunsuk <71748139+Yeunsuk@users.noreply.github.com>
2025-05-01 13:24:49 +0900

    [Silver I] Title: 싫은데요, Time: 188 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/25916. 싫은데요/싫은데요.java
+++ 백준/Silver/25916. 싫은데요/싫은데요.java
@@ -2,17 +2,19 @@ import java.util.*; //25916(두 포인터) 싫은데요
 import java.io.*;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int n = Integer.parseInt(st.nextToken());
-    int m = Integer.parseInt(st.nextToken());
-    int sum = 0, rlt = 0, a = 0;
-    int[] arr = new int[n];
+  static int read() throws Exception {
+    int c, n = System.in.read() & 15;
+    while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+    return n;
+  }
   
-    st = new StringTokenizer(br.readLine());
-    for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+  public static void main(String[] args) throws Exception {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int sum = 0, rlt = 0, a = 0;
+    int n = read(), m = read();
+    
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++) arr[i] = read();
 
     for(int i = 0; i < n; i++) {
       sum += arr[i];
