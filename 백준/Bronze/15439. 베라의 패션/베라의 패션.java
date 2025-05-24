e0bcb4afe506a68f8b5253a1e7bb7bfd97f213e0
eunsuk <71748139+Yeunsuk@users.noreply.github.com>
2025-05-24 22:54:32 +0900

    Update 베라의 패션.java

--- 백준/Bronze/15439. 베라의 패션/베라의 패션.java
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -1,2 +1,2 @@
 //15439(수학) 베라의 패션
-class Main{public static void main(String[]a){int n=new java.util.Scanner(System.in).nextInt();System.out.print(n*--n);}}
+interface Main{static void main(String[]a){int n=new java.util.Scanner(System.in).nextInt();System.out.print(n*--n);}}
