f58b23258d04432139876cbde86066756ca636d8
eunsuk <71748139+Yeunsuk@users.noreply.github.com>
2025-11-29 15:54:10 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.java
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.java
@@ -2,9 +2,7 @@ import java.util.*; //25418(graphs) 정수 a를 k로 만들기
 import java.io.*;
 
 public class Main {
-  static int stoi(String s) { return Integer.parseInt(s); }
   public static void main(String[] args) throws IOException {
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
     int n = Integer.parseInt(st.nextToken()), cnt = 0;
@@ -16,7 +14,6 @@ public class Main {
       cnt++;
     }
     
-    bw.write(String.valueOf(cnt + n - k));
-    bw.close();
+    System.out.print(cnt + n - k);
   }
 }
