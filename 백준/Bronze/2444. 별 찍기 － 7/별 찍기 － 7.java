979aada55fb5e7b8415e477d8cd86b32aec5602a
eunsuk <71748139+Yeunsuk@users.noreply.github.com>
2025-12-16 11:45:01 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 76 ms, Memory: 11800 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,26 @@
+import java.util.*; //2444(DP) 별 찍기 - 7
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    
+    for(int i = 1; i <= n; i++) {
+      for(int j = 1; j <= n-i; j++) sb.append(" ");
+      for(int j = 1; j <= 2*i-1; j++) sb.append("*");
+      sb.append("\n");
+    }
+    
+    for(int i = n-1; i >= 1; i--) {
+      for(int j = 1; j <= n-i; j++) sb.append(" ");
+      for(int j = 1; j <= 2*i-1; j++) sb.append("*");
+      sb.append("\n");
+    }
+    
+    bw.write(sb.toString());
+    bw.close();
+  }
+}
