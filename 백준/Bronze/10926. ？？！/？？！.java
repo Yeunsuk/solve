fa7902aef297ef2a3c72c1b96e1e88aafa4ecbe9
eunsuk <71748139+Yeunsuk@users.noreply.github.com>
2026-01-08 10:10:37 +0900

    [Bronze V] Title: ??!, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,6 @@
+import java.io.*; //10926(구현) ??!
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.print(br.readLine() + "??!");}}
