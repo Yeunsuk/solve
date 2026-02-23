59d5957fe6bbb270e2b35bc526d72ecada68903c
eunsuk <71748139+Yeunsuk@users.noreply.github.com>
2026-02-23 23:03:24 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 56 ms, Memory: 11460 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,7 @@
+import java.io.*; //14581 팬들에게 둘러싸인 홍준
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.print(":fan::fan::fan:\n:fan::" + br.readLine() + "::fan:\n:fan::fan::fan:");
+  }
+}
