af6aec8b5db7fcd13c7edc261d19c47b1ab38165
eunsuk <71748139+Yeunsuk@users.noreply.github.com>
2026-02-27 16:33:16 +0900

    [Bronze V] Title: CAPS, Time: 200 ms, Memory: 23764 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,8 @@
+import java.io.*; //15000 CAPS
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.print(br.readLine().toUpperCase());
+  }
+}
