ed07337322b838c8dc18106adbc887366dc74f43
eunsuk <71748139+Yeunsuk@users.noreply.github.com>
2026-04-11 13:18:48 +0900

    [Bronze V] Title: 제리와 톰, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,12 @@
+import java.io.*; //16430 제리와 톰
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int a = Integer.parseInt(st.nextToken());
+    int b = Integer.parseInt(st.nextToken());
+    System.out.print(b-a + " " + b);
+  }
+}
