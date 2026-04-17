432d0e74621eadd52bc67cf76de894a6110a2801
eunsuk <71748139+Yeunsuk@users.noreply.github.com>
2026-04-17 10:35:58 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 72 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+    int k = Integer.parseInt(st.nextToken());
+    
+    while(k-->0) if(n >= m*2) n--; else m--;
+    System.out.print(Math.min(m, n/2));
+  }
+}
