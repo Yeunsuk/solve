fb4eb2ef150fe956cfe5385abe1764f6cc03bc16
eunsuk <71748139+Yeunsuk@users.noreply.github.com>
2026-04-18 13:52:28 +0900

    [Bronze V] Title: 홍익대학교, Time: 64 ms, Memory: 11392 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,8 @@
+import java.io.*; //16394 홍익대학교
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.print(Integer.parseInt(br.readLine())-1946);
+  }
+}
